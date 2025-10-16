387864ddfc21a7dac28b02d81a7cba20a12a6e29
VenancioGarridoMoya <venancio.garrido@iesleonardodavinci.com>
2025-10-16 10:02:22 +0200

    commit4

---
+++ Tema1/ejemplosT1/ejemplo3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+
+    printf("Introduce un numero entero: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("El factorial no esta definido para numeros negativos.\n");
+    } else {
+        for (i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("El factorial de %d es %llu\n", n, factorial);
+    }
+
+    return 0;
+}
