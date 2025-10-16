8cf33ff77f4265a5afff15354012bd09a0d46a20
VenancioGarridoMoya <venancio.garrido@iesleonardodavinci.com>
2025-10-16 09:51:33 +0200

    commit2

---
+++ Tema1/ejemplosT1/ejemplo2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+int main() {
+    char cadena[MAX_LEN];
+
+    printf("Introduce una cadena: ");
+    fgets(cadena, MAX_LEN, stdin);
+
+    printf("La cadena introducida es: %s", cadena);
+
+    return 0;
+}
