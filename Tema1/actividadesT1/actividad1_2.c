bccec5d6e56578e05c396bff26e7b4dcd7a3cf14
Venancio Garrido <venancio.garrido@iesleonardodavinci.com>
2025-11-06 09:21:08 +0100

    actividad1_2

--- Tema1/actividadesT1/actividad1_2.c
+++ Tema1/actividadesT1/actividad1_2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,7 +10,7 @@ int main(){
     pid_t pid;//guardaremos el id del proceso hijo
     int salida;
     char quienSoy[15];
-
+    char mensajeDelHijo[]="Hola papi te estoy enviando este mensaje.\0";
 
     pipe(fd); //creamos el pipe
 
@@ -22,16 +22,18 @@ int main(){
             salida = -1;
             break;
         case 0: // Proceso hijo
+            close(fd[0]); //cierra el descriptor de lectura
             strcpy(quienSoy,"Hijo");
             printf("El proceso HIJO escribe en la tubería...\n"); //mensaje qu escribe el hijo en el pipe
-            write(fd[1], "Hola papi", 10);
+            write(fd[1], mensajeDelHijo, strlen(mensajeDelHijo));
             salida=1;
             break;
         default:
+            close(fd[1]); //cierra el descriptor de escritura
             strcpy(quienSoy,"Padre");
             wait(NULL); //el padre espera a que termine el hijo
             printf("El proceso PADRE lee de la tubería...\n");
-            read(fd[0], buffer, 10);
+            read(fd[0], buffer, strlen(mensajeDelHijo));
             printf("\nEl mensaje es: %s\n", buffer); //el padre lee lo que el hijo ha escrito
             salida=1;
             break;
