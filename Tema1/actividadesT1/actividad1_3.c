d813d0ca045b5a2c8bc0b1ba2a17e7a8f1679aea
Venancio Garrido <venancio.garrido@iesleonardodavinci.com>
2025-11-27 09:43:00 +0100

    actividad1_3

--- Tema1/actividadesT1/actividad1_3.c
+++ Tema1/actividadesT1/actividad1_3.c
@@ -4,10 +4,15 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+//variable global para contar señales recibidas
+int contadorSenales=0;
+int fd[2];
+
 
 // Padre manejador para la señal SIGUSR1
 void manejadorPadre(int senal){
-    printf("\nEl PADRE recibe la señal ... %d proceso: %d).\n",senal,getpid());
+    read(fd[0],&contadorSenales,sizeof(int)); // padre lee contador del pipe
+    printf("\n%d El PADRE (%d) recibe la señal %d \n",contadorSenales+1, getpid(), senal);
 }
 
 //principal
@@ -15,9 +20,13 @@ int main(){
     __pid_t pidProceso,pidHijo, pidPadre;
     int salida=0, i=0;
     char caracter=0;
+    int numSenales=0;
+    pipe(fd);// pipe para enviar/recibir el contador de señales
+    printf("¿Qué número de señales quieres enviar al padre: ");
+    scanf("%d", &numSenales);
+
     pidPadre=getpid();
     pidProceso = fork();
-    
     switch (pidProceso) {
     case -1:
         printf("Error, no se pudo crear el proceso");
@@ -25,16 +34,17 @@ int main(){
         break;
     case 0: // estoy en el proceso hijo
         sleep(1);
-        for (i=0;i<10;i++){
+        for (contadorSenales=0;contadorSenales<numSenales;contadorSenales++){
+            write(fd[1],&contadorSenales,sizeof(int)); // hijo escribe contador en pipe
             kill(pidPadre, SIGUSR1); // hijo envía señal al padre
             sleep(1);
         }
-        kill (pidPadre,SIGKILL);
+        kill (pidPadre,SIGKILL); // hijo mata al padre
         break;
     default: //estoy en el proceso padre (el valor es el de el pid del hijo)
         signal(SIGUSR1,manejadorPadre);    
         while(1){// no hace nada...espera su muerte
-            pause();
+            pause(); // espera señal
         }
         break;
     }
